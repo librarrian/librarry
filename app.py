343e0def74c8974bc2da06a146092681a5435d0b
librarrian <librarrian@proton.me>
2026-04-20 19:17:58 -0600

    updates to app

--- app.py
+++ app.py
@@ -1,33 +1,33 @@
-from flask import Flask, request, jsonify, wrappers
-import os
 import logging
 import json
-import requests
-import sys
-
-
-from utils import qbittorrent_interface, constants, logging_setup
-from utils.import_to_library import import_books
+from flask import Flask, request, jsonify, wrappers, abort, send_from_directory
+from utils import constants, logging_setup
 
 logging_setup.run()
+from utils import import_to_library, qbittorrent_interface
+
+
 constants.validate_env()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=constants.LOG_DIR)
 
 app.logger.propagate = True
 logger = logging.getLogger(__name__)
 
 
-def get_torrent_info(
-    request: wrappers.Request, torrent_info: dict
-) -> tuple[wrappers.Response, int] | None:
-    hash = request.args.get("hash_v1")
-    if not hash:
-        hash = request.args.get("hash_v2")
-    if not hash:
+class ImmediateResponse(Exception):
+    def __init__(self, response: wrappers.Response, code: int):
+        self.response = (response, code)
+
+
+def get_hash(request: wrappers.Request) -> str:
+    torrent_hash = request.args.get("hash_v1")
+    if not torrent_hash:
+        torrent_hash = request.args.get("hash_v2")
+    if not torrent_hash:
         message = "Neither hash_v1 nor hash_v2 not found in torrent_complete request. Add as url argument, e.g. /torrent_complete?hash_v1=abc123"
         logger.error(message)
-        return (
+        raise ImmediateResponse(
             jsonify(
                 {
                     "status": "error",
@@ -36,14 +36,18 @@ def get_torrent_info(
             ),
             400,
         )
+    return torrent_hash
+
+
+def get_torrent_info(torrent_hash: str) -> dict:
     tor_interface = qbittorrent_interface.QBittorrentInterface(
         address=constants.QBITTORRENT_ADDRESS
     )
     try:
-        torrent_info.update(tor_interface.get_torrent_info(hash))
+        torrent_info = tor_interface.get_torrent_info(torrent_hash)
     except qbittorrent_interface.QbittorrentError as e:
         logger.error(e)
-        return (
+        raise ImmediateResponse(
             jsonify(
                 {
                     "status": "error",
@@ -51,21 +55,38 @@ def get_torrent_info(
                 }
             ),
             500,
-        )
+        ) from e
     if "name" not in torrent_info:
         torrent_info["name"] = ""
-    return
+    if not torrent_info.get("category") == "audiobook":
+        raise ImmediateResponse(
+            jsonify(
+                {
+                    "status": "success",
+                    "message": f"Non audiobook torrent: {torrent_info["name"]}",
+                }
+            ),
+            200,
+        )
+    return torrent_info
 
 
 @app.route("/torrent_added", methods=["POST"])
 def torrent_added():
-    torrent_info = {}
-    response = get_torrent_info(request, torrent_info)
-    if response:
-        return response
+    logger.info(
+        "\n\n\n----------------------------------------------------TORRENT ADDED----------------------------------------------------"
+    )
+    try:
+        torrent_hash = get_hash(request)
+        import_to_library.mark_processing(torrent_hash)
+        torrent_info = get_torrent_info(torrent_hash)
+    except ImmediateResponse as e:
+        return e.response
+
     name = torrent_info["name"]
-    logger.info(f"Torrent found: {name}")
-    logger.debug(f"Torrent info: {json.dumps(torrent_info, indent=2)}")
+    logger.info("Torrent found: %s", name)
+    logger.debug("Torrent info: %s", {json.dumps(torrent_info, indent=2)})
+    import_to_library.save_book_data(torrent_info)
     return (
         jsonify({"status": "success", "message": f"Audiobook data found: {name}"}),
         200,
@@ -74,14 +95,17 @@ def torrent_added():
 
 @app.route("/torrent_complete", methods=["POST"])
 def torrent_complete():
-    torrent_info = {}
-    response = get_torrent_info(request, torrent_info)
-    if response:
-        return response
-
+    logger.info(
+        "\n\n\n----------------------------------------------------TORRENT COMPLETE----------------------------------------------------"
+    )
+    try:
+        torrent_hash = get_hash(request)
+        torrent_info = get_torrent_info(torrent_hash)
+    except ImmediateResponse as e:
+        return e.response
     name = torrent_info["name"]
-    logger.info(f"Torrent found: {name}")
-    logger.debug(f"Torrent info: {json.dumps(torrent_info, indent=2)}")
+    logger.info("Torrent found: %s", name)
+    logger.debug("Torrent info: %s", {json.dumps(torrent_info, indent=2)})
     if not torrent_info.get("category") == "audiobook":
         return (
             jsonify({"status": "success", "message": f"Non audiobook torrent: {name}"}),
@@ -95,7 +119,7 @@ def torrent_complete():
         "0": False,
     }.get(request.args.get("overwrite", "false").lower(), False)
     try:
-        import_books(torrent_info, overwrite=overwrite)
+        import_to_library.import_books(torrent_info, overwrite=overwrite)
     except Exception as e:
         return (
             jsonify({"status": "error", "message": f"{e}"}),
@@ -111,6 +135,23 @@ def torrent_complete():
 @app.route("/health_check", methods=["GET"])
 def health_check():
     return jsonify({"status": "success", "message": "Server is healthy"}), 200
+
+
+@app.route("/logs/<level>")
+# @app.route("/logs/<level>/<duplicate>")
+# def logs(level: str, duplicate: int):
+def logs(level: str):
+    file = {
+        "debug": "debug.log",
+        "info": "info.log",
+        "warning": "warning.log",
+        "error": "error.log",
+        "critical": "critical.log",
+    }.get(level.lower())
+    if file:
+        return send_from_directory(constants.LOG_DIR, file)
+    else:
+        abort(404)
 
 
 if __name__ == "__main__":
